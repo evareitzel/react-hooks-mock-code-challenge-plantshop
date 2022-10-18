9b94977f6d6f11dac535e9c17bfdf6ca54f03be4
Eva Reitzel <evareitzel@gmail.com>
2022-10-17 22:21:17 -0400

    Done.

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,14 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({plants}) {
+  const renderPlants = plants.map(plant => {
+      return <PlantCard image={plant.image} name={plant.name} price={plant.price} key={plant.name}/>
+    })
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">{/* render PlantCards components in here */}
+    {renderPlants}</ul>
   );
 }
 
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([])
+  const [search, setSearch] = useState("")
+
+  useEffect(() => {
+    fetch('http://localhost:6001/plants')
+    .then(r => r.json())
+    .then(plantData => setPlants(plantData))
+  }, [])
+
+  function handleAddPlant(newPlant){
+    console.log('In PlantPage: ', newPlant)
+    setPlants([...plants, newPlant])
+  }
+
+    console.log(search)
+    const filtered = plants.filter(plants =>plants.name.toLowerCase().includes(search))
+
+    console.log(filtered)
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant}/>
+      <Search setSearch={setSearch}/>
+      <PlantList plants={filtered} />
     </main>
   );
 }
